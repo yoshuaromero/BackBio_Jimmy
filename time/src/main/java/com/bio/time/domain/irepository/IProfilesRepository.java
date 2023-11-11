596a88540c695dcd78aad4744538724f706ad0d1
yoshuaromero <yrom532@hotmail.com>
2023-11-11 11:12:57 -0500

    Delete time directory

--- time/src/main/java/com/bio/time/domain/irepository/IProfilesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bio.time.domain.irepository;
-
-import com.bio.time.persistence.entity.ProfilesEntity;
-
-import java.util.Optional;
-
-public interface IProfilesRepository {
-    Optional<ProfilesEntity> findById(Integer idProfile);
-}
